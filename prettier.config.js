3de74aab6b716da162f32b31c0db5a528f257ea4
ChrisCoy <christopherleegs@outlook.com>
2023-02-25 20:55:17 -0300

    ♻chore: set up prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  bracketSpacing: true,
+  useTabs: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 4,
+  semi: true,
+  arrowParens: "avoid",
+  printWidth: 80,
   plugins: [require("prettier-plugin-tailwindcss")],
 };
--- src/components/layout/Container/index.tsx
+++ src/components/layout/Container/index.tsx
@@ -1,18 +1,18 @@
 import { ReactNode } from "react";
 
 interface ContainerProps {
-  children?: ReactNode;
-  className?: string;
+	children?: ReactNode;
+	className?: string;
 }
 
 function Container({ children, className = "" }: ContainerProps) {
-  return (
-    <div
-      className={`mx-auto flex w-full max-w-screen-xl gap-4 px-4 ${className}`}
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			className={`mx-auto flex w-full max-w-screen-xl gap-4 px-4 ${className}`}
+		>
+			{children}
+		</div>
+	);
 }
 
 export { Container };
